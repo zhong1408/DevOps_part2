1a1d0258b1b1f76833f77582f3ee922b85ef03f4
zhong1408 <chiayizhong@gmail.com>
2024-11-07 20:57:17 +0800

    implemented backend

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-var express = require("express");
+var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
-const PORT = process.env.PORT || 5050;
+
+const PORT = process.env.PORT || 5050
 var startPage = "index.html";
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/" + startPage);
-});
+
+const { viewCourses,updateCourse } = require('./utils/UpdateCourseUtil.js');
+app.get('/view-courses', viewCourses);
+app.put('/update-course/:id', updateCourse);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/" + startPage);
+})
+
 server = app.listen(PORT, function () {
-  const address = server.address();
-  const baseUrl = `http://${
-    address.address == "::" ? "localhost" : address.address
-  }:${address.port}`;
-  console.log(`Demo project at: ${baseUrl}`);
+    const address = server.address();
+    const baseUrl = `http://${address.address == "::" ? 'localhost' :
+        address.address}:${address.port}`;
+    console.log(`Demo project at: ${baseUrl}`);
 });
-module.exports = { app, server };
+
+module.exports = { app, server }
