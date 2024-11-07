4572b3a7877151aaab5b7f135d33d5f5fc2327ad
mhk28 <muhammadhasankhan374@gmail.com>
2024-11-07 20:59:23 +0800

    Implemented backend for add course

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+
 const PORT = process.env.PORT || 5050;
+
 var startPage = "index.html";
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+const { addCourse } = require('./utils/AddCourseUtil_hasan.js')
+app.post('/add-course', addCourse)
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/" + startPage);
 });
+
 server = app.listen(PORT, function () {
   const address = server.address();
   const baseUrl = `http://${
@@ -16,4 +24,7 @@ server = app.listen(PORT, function () {
   }:${address.port}`;
   console.log(`Demo project at: ${baseUrl}`);
 });
+
+
 module.exports = { app, server };
+
