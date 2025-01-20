261ef7cac5c1bfa2d3354fd6abd832de331ab835
zhong1408 <chiayizhong@gmail.com>
2025-01-20 16:22:50 +0800

    Completed the implementation of real-time monitoring and logging

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
-
+const logger = require('./logger');
 
 const PORT = process.env.PORT || 5050;
 
@@ -11,8 +11,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
 
+const statusMonitor = require('express-status-monitor');
+app.use(statusMonitor());
 
-const { viewCourses,updateCourse } = require('./utils/UpdateCourseUtil.js');
+
+const { viewCourses, updateCourse } = require('./utils/UpdateCourseUtil.js');
 app.get('/view-courses', viewCourses);
 app.put('/update-course/:id', updateCourse);
 
@@ -22,10 +25,12 @@ app.get("/", (req, res) => {
 });
 
 server = app.listen(PORT, function () {
-    const address = server.address();
-    const baseUrl = `http://${address.address == "::" ? 'localhost' :
-        address.address}:${address.port}`;
-    console.log(`Demo project at: ${baseUrl}`);
+  const address = server.address();
+  const baseUrl = `http://${address.address == "::" ? 'localhost' :
+    address.address}:${address.port}`;
+  console.log(`Demo project at: ${baseUrl}`);
+  logger.info(`Demo project at: ${baseUrl}!`);
+  logger.error(`Example or error log`)
 });
 
 
