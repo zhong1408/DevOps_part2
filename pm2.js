d4fd730741083a8372ecba47654eeaddfa2cd54a
zhong1408 <chiayizhong@gmail.com>
2025-02-10 13:22:38 +0800

    okay

--- pm2.js
+++ pm2.js
@@ -2,14 +2,14 @@ module.exports = {
   apps: [
     {
       name: "resource-mgmt",
-      script: "index.js", // Main application entry file
-      instances: "max", // Uses all available CPU cores
+      script: "index.js", 
+      instances: "max", 
       watch: true, // Restarts on file changes
       autorestart: true, // Ensures automatic restart on failure
       log_date_format: "YYYY-MM-DD HH:mm:ss",
-      error_file: "logs/error.log", // PM2 error log file
-      out_file: "logs/combined.log", // PM2 combined output file
-      time: true, // Adds timestamp to logs
+      error_file: "logs/error.log", 
+      out_file: "logs/combined.log", 
+      time: true, 
     },
   ],
 };
